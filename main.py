24be769495a92d4f37fb09ba123b928901f19b3c
DairyProducts <66636627+DairyProducts@users.noreply.github.com>
2022-10-26 15:31:32 -0700

    Finished Progress Bar

--- main.py
+++ main.py
@@ -27,8 +27,10 @@ errlbl.place(y=120)
 progressbar=ttk.Progressbar(window, orient=HORIZONTAL, length=200, mode='determinate')
 progressbar.place(x=175, y=200, anchor="center"), 
 
+
 # Video download function and error reporting
 def clicked():
+        global filesize
         # progressbar['value']=0
         lbl.config(text="Downloading...", fg="orange")
         filepath = filedialog.askdirectory()
@@ -36,12 +38,11 @@ def clicked():
         
         try:
                 # Download the video
-                yt = YouTube(url, on_progress_callback=progress)
-                yt.streams.filter(file_extension='mp4')
+                yt = YouTube(url)
+                filesize = yt.streams.filter(file_extension='mp4', progressive=True).get_highest_resolution().filesize
                 # Register the callback function
                 yt.register_on_progress_callback(progress)
-                window.update_idletasks()
-                yt.streams.get_highest_resolution().download(filepath)
+                yt.streams.filter(file_extension='mp4', progressive=True).get_highest_resolution().download(filepath)
                 lbl.config(text="Downloaded", fg="green")
         except Exception as e:
                 # Update error label text
@@ -50,19 +51,27 @@ def clicked():
                 errlbl.place_configure(y=135, x=175, anchor="center")
                 lbl.config(text="Failed", fg="red")
 
+
 # Update progress bar with video download progress
-def progress(stream, chunk, bytes_remaining):
-        total_size = stream.filesize
-        bytes_downloaded = total_size - bytes_remaining
-        progress = round((bytes_downloaded / total_size) * 100, 2)
-        progressbar['value']=progress
-        window.update_idletasks()
+def progress(chunk, file_handle, bytes_remaining):
+        remaining = (100 * bytes_remaining) / filesize
+        step = 100 - int(remaining)
+        print(step)
+        progressbar['value'] = step
+        progressbar.update()
 
 # Create download button
 btn = Button(window, text="Download", command=clicked)
 btn.pack()
 note = Label(window, text="Note: at the moment, the video will be downloaded \nin the highest resolution available, capping out at 720p.")
 note.pack()
+
+# Add button that links to Github repo
+def openGithub():
+        import webbrowser
+        webbrowser.open_new("github.com/dairyproducts/ytdownloader")
+ghbtn = Button(window, text="📂", command=openGithub)
+ghbtn.place(x=345, y=295, anchor="se")
 
 metadata = Label(window, text="Version 1.0.0b")
 metadata.place(x=5, y=295, anchor="sw")
